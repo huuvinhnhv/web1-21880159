b85fc83a1db2d127f9009e80488bce8bdbeb68a7
Nguyễn Hữu Vinh <huuvinhnhv@gmail.com>
2022-10-12 10:40:14 +0700

    login + comment

--- js/21880159.js
+++ js/21880159.js
@@ -10,14 +10,7 @@ async function loadData(request, templateId, viewId) {
   var view = document.getElementById(viewId);
   view.innerHTML = template(context);
 }
-function setActiveImagepath(img, imagepath, isActive = true) {
-  if (isActive) {
-    img.src = imagepath.replace(".", "-active.");
-  } else {
-    img.src = imagepath;
-  }
-  img.nextElementSibling.classList.toggle("web1-text-blue");
-}
+
 function disableLink(link) {
   let links = document.querySelectorAll("#gallery-categories a");
   for (let i = 0; i < links.length; i++) {
@@ -28,16 +21,80 @@ function disableLink(link) {
 
 async function getAuthenticateToken(username, password) {
   let response = await fetch(`${AUTHENTICATE_API}/authenticate`, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
     },
     body: JSON.stringify({ username, password }),
   });
-  let result = response.json();
+  let result = await response.json();
   if (response.status == 200) {
     return result.token;
   }
   throw new Error(result.message);
+}
+
+async function login(e) {
+  e.preventDefault();
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+  document.getElementById("error-login-message").innerHTML = '';
+  try {
+    let token = await getAuthenticateToken(username, password);
+    if (token) {
+      localStorage.setItem('token', token);
+      document.getElementsByClassName('btn-close')[0].click();
+      displayControls();
+    }
+  } catch (error) {
+    document.getElementById("error-login-message").innerHTML = error;
+    displayControls(false);
+  }
+}
+
+function displayControls(isLogin = true) {
+  let linkLogins = document.getElementsByClassName('link-login');
+  let linkLogouts = document.getElementsByClassName('link-logout');
+  let displayLogin = 'none';
+  let displayLogout = 'block';
+  if (!isLogin) {
+    displayLogin = 'block';
+    displayLogout = 'none';
+  }
+  console.log(isLogin)
+  for (let i = 0; i < 2; i++) {
+    linkLogins[i].style.display = displayLogin;
+    linkLogouts[i].style.display = displayLogout;
+  }
+  let leaveComment = document.getElementById('leave-comment');
+  if (leaveComment) {
+    leaveComment.style.display = displayLogout;
+  }
+}
+
+async function checkLogin() {
+  let isLogin = await verifyToken();
+  displayControls(isLogin);
+}
+async function verifyToken() {
+  let token = localStorage.getItem('token');
+  if (token) {
+    let response = await fetch(`${AUTHENTICATE_API}/verify`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': 'Bearer ' + token
+      }
+    });
+    if (response.status == 200) {
+      return true;
+    }
+  }
+  return false;
+}
+function logout() {
+  localStorage.clear();
+  displayControls(false);
 }
